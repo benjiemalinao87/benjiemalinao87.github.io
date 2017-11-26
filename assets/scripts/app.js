3c8cf18c7a9946ff4a37b5c8454119ea103faf12
Benjie Malinao <benjiemalinao87@gmail.com>
2017-11-26 01:51:00 +0000

    work experience updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,8 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"AWS Certified Solution Architect.", "Microsoft Cisco VMware Certified.", "DevOps| SysOps"
+				
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
